691734d127762193ffcf3620393db57fcf470b34
SeYn0604 <lkjn111@naver.com>
2024-02-01 08:41:54 +0900

    02.01.
    조준사격 구현

--- Vamseo/Assets/0.Script/PlayerAgent.cs
+++ Vamseo/Assets/0.Script/PlayerAgent.cs
@@ -70,28 +70,18 @@ public class PlayerAgent : Agent
         
         nextMove.x = actions.ContinuousActions[0]*3;
         nextMove.y = actions.ContinuousActions[1]*3;
-        /*
-        nextShot.x = (actions.ContinuousActions[2]+this.transform.position.x);
-        nextShot.y = (actions.ContinuousActions[3]+this.transform.position.y);*/
-        //nextShot.z = actions.ContinuousActions[2];
-        nextShot = Mathf.Clamp(actions.ContinuousActions[2], -1f, 1f) * 180f; // -1과 1 사이의 값을 -180과 180 사이의 각도로 변환
         transform.Translate(nextMove * Time.deltaTime * speed);
 
-        if (bullettimer > 1.5f ) // && rayOutput.HitGameObject.CompareTag("Monster"))눌레퍼런스뜸 ㅜㅜ
+        if (bullettimer > 2.0f)
         {
             GameObject detectedMonster = RayCastInfo(m_rayPerceptionSensorComponent2D);
-            if (detectedMonster != null)
+            if (detectedMonster != null && detectedMonster.CompareTag("Monster"))
             {
                 Vector2 targetDirection = detectedMonster.transform.position - transform.position;
                 CreateBullet(targetDirection.normalized);
                 bullettimer = 0;
             }
         }
-        
-        /*
-        Vector3 velocity = new Vector3(actions.ContinuousActions[0]/100, actions.ContinuousActions[1]/100, 0);
-        rb.AddForce(velocity, ForceMode2D.Impulse);
-        */
     }
 
     public void Destroymonsters() //Endepisode용
@@ -115,7 +105,7 @@ public class PlayerAgent : Agent
         if (other.gameObject.CompareTag("Monster")) 
         {
             Debug.Log("impact monster");
-            player.Hit(10);
+            player.Hit(110);
             AddReward(-1.0f);
             Destroy(other.gameObject); //여기까지는 에이전트가 죽지않았을 때, 피 일부와 보상점수 일부 차감
             if(player.HP <= 0f)
@@ -152,9 +142,6 @@ public class PlayerAgent : Agent
                 GameObject goHit = rayOutputs[i].HitGameObject;
                 if (goHit != null)
                 {
-                    // Found some of this code to Denormalized length
-                    // calculation by looking trough the source code:
-                    // RayPerceptionSensor.cs in Unity Github. (version 2.2.1)
                     var rayDirection = rayOutputs[i].EndPositionWorld - rayOutputs[i].StartPositionWorld;
                     var scaledRayLength = rayDirection.magnitude;
 
@@ -164,13 +151,13 @@ public class PlayerAgent : Agent
                         minlength = rayHitDistance;
                         minnum = i;
                     }
-                    // Print info:
+                    /*잘되나 확인
                     string dispStr;
                     dispStr = "__RayPerceptionSensor - HitInfo__:\r\n";
                     dispStr = dispStr + "GameObject name: " + goHit.name + "\r\n";
                     dispStr = dispStr + "GameObject tag: " + goHit.tag + "\r\n";
                     dispStr = dispStr + "Hit distance of Ray: " + rayHitDistance + "\r\n";
-                    //Debug.Log(dispStr);
+                    Debug.Log(dispStr);*/
                 }
             }
             return rayOutputs[minnum].HitGameObject;
